7b5ffbcd3168d8641a17ec92fb8da3a18261db59
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-16 17:49:58 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -18,11 +18,11 @@ async def welcome(event: NewMessage.Event | Message):
         ]
     )
 
-@TelegramBot.on(NewMessage(incoming=True, pattern=r'^/info$'))
+@TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/info$'))
 @verify_user(private=True)
 async def user_info(event: Message):
     await event.reply(UserInfoText.format(sender=event.sender))
-
-@TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/log$'))
+    
+@TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/logs$'))
 async def send_log(event: NewMessage.Event | Message):
     await event.reply(file='event-log.txt')
