3afa253889070e485c358e68be6c511e58d25bd4
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-22 02:22:35 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,4 +1,5 @@
-from telethon import Button
+import re
+from telethon import Button, events
 from telethon.events import NewMessage
 from telethon.tl.custom.message import Message
 from bot import TelegramBot
@@ -9,32 +10,33 @@ from bot.database import db
 
 @TelegramBot.on(NewMessage(incoming=True, pattern=r'^/start$'))
 @verify_user(private=True)
-async def welcome(event: NewMessage.Event | Message):
-    if await db.is_inserted("ban", event.sender.id):
+async def welcome(event: Message):
+    if await db.is_inserted("ban", event.sender_id):
         return await event.reply("You are banned")
     await event.reply(
         message=WelcomeText % {'first_name': event.sender.first_name}
     )
-    if not await db.is_inserted("users", event.sender.id):
-        await db.insert("users", event.sender.id)
+    if not await db.is_inserted("users", event.sender_id):
+        await db.insert("users", event.sender_id)
 
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/info$'))
 @verify_user(private=True)
 async def user_info(event: Message):
     await event.reply(UserInfoText.format(sender=event.sender))
-    
+
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/logs$'))
-async def send_log(event: NewMessage.Event | Message):
+async def send_log(event: Message):
     await event.reply(file='event-log.txt')
-    
+
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/users$'))
 @verify_user(private=True)
 async def users(event: Message):
-    await event.reply('Total Users Count:', len(db.fetch_all("users")))
+    total_users = len(await db.fetch_all("users"))
+    await event.reply(f'Total Users Count: {total_users}')
 
-@TelegramBot.on(events.NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/ban (\d+)$'))
+@TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/ban (\d+)$'))
 @verify_user(private=True)
-async def ban(event: Message):
+async def ban_user(event: Message):
     match = re.match(r'^/ban (\d+)$', event.raw_text)
     if not match:
         await event.reply("Please provide a valid user ID.")
@@ -42,11 +44,11 @@ async def ban(event: Message):
     user_id = int(match.group(1))
     if not await db.is_inserted("ban", user_id):
         await db.insert("ban", user_id)
-    await event.reply('Done banned!')
+    await event.reply('User banned!')
 
-@TelegramBot.on(events.NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/unban (\d+)$'))
+@TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/unban (\d+)$'))
 @verify_user(private=True)
-async def ban(event: Message):
+async def unban_user(event: Message):
     match = re.match(r'^/unban (\d+)$', event.raw_text)
     if not match:
         await event.reply("Please provide a valid user ID.")
@@ -61,23 +63,23 @@ async def ban(event: Message):
 async def bcast(event: Message):
     if not event.reply_to_msg_id:
         return await event.reply(
-            "Please use `/broadcast` as reply to the message you want to broadcast."
+            "Please use `/bcast` as reply to the message you want to broadcast."
         )
     msg = await event.get_reply_message()
     xx = await event.reply("In progress...")
     users = await db.fetch_all('users')
     done = error = 0
-    for i in users:
+    for user_id in users:
         try:
             await TelegramBot.send_message(
-                int(i),
-                msg.text.format(user=(await TelegramBot.get_entity(int(i))).first_name),
+                int(user_id),
+                msg.text.format(user=(await TelegramBot.get_entity(int(user_id))).first_name),
                 file=msg.media,
                 buttons=msg.buttons,
                 link_preview=False,
             )
             done += 1
         except Exception as brd_er:
-            log.error("Broadcast error:\nChat: %d\nError: %s", int(i), brd_er)
+            log.error("Broadcast error:\nChat: %d\nError: %s", int(user_id), brd_er)
             error += 1
-    await xx.edit("Broadcast completed.\nSuccess: {}\nFailed: {}".format(done, error))
+    await xx.edit(f"Broadcast completed.\nSuccess: {done}\nFailed: {error}")
