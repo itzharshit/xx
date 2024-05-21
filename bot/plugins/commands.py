f1f412c40e28d52e16221deeaa286c9390ece789
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-22 01:55:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -5,18 +5,18 @@ from bot import TelegramBot
 from bot.config import Telegram
 from bot.modules.static import *
 from bot.modules.decorators import verify_user
-from bot.database import Database 
+from bot.database import db
 
 @TelegramBot.on(NewMessage(incoming=True, pattern=r'^/start$'))
 @verify_user(private=True)
 async def welcome(event: NewMessage.Event | Message):
-    if await Database.is_inserted("users", event.sender.id):
+    if await db.is_inserted("ban", event.sender.id):
         return await event.reply("You are banned")
     await event.reply(
         message=WelcomeText % {'first_name': event.sender.first_name}
     )
-    if not await Database.is_inserted("users", event.sender.id):
-        await Database.insert("users", event.sender.id)
+    if not await db.is_inserted("users", event.sender.id):
+        await db.insert("users", event.sender.id)
 
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/info$'))
 @verify_user(private=True)
@@ -30,22 +30,22 @@ async def send_log(event: NewMessage.Event | Message):
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/users$'))
 @verify_user(private=True)
 async def users(event: Message):
-    await event.reply('Total Users Count:', len(Database.fetch_all("users")))
+    await event.reply('Total Users Count:', len(db.fetch_all("users")))
 
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/ban$'))
 @verify_user(private=True)
 async def ban(event: Message):
     user_id = 
-    if not await Database.is_inserted("ban", user_id):
-        await Database.insert("ban", user_id)
+    if not await db.is_inserted("ban", user_id):
+        await db.insert("ban", user_id)
     await event.reply('Done banned!')
 
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/unban$'))
 @verify_user(private=True)
 async def unban(event: Message):
     user_id =
-    if await Database.is_inserted("ban", user_id):
-        await Database.delete("ban", user_id)
+    if await db.is_inserted("ban", user_id):
+        await db.delete("ban", user_id)
     await event.reply('User unbanned!')
 
 @TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/bcast$'))
@@ -57,7 +57,7 @@ async def bcast(event: Message):
         )
     msg = await event.get_reply_message()
     xx = await event.reply("In progress...")
-    users = await Database.fetch_all('users')
+    users = await db.fetch_all('users')
     done = error = 0
     for i in users:
         try:
